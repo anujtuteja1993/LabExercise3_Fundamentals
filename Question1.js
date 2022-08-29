4ddec7abbbf84479300db2e6960cf76eefc13e75
Anuj Tuteja <anujtuteja@Anujs-MBP.fritz.box>
2022-08-29 20:43:45 +1200

    Initial Commit

---
+++ Question1.js
@@ -0,0 +1,57 @@
+//What are the results of these expressions?
+
+//"" + 1 + 0
+let var1 = " " + 1 + 0;
+console.log(var1);
+
+//" " - 1 + 0
+let var2 = " " - 1 + 0;
+console.log(var2);
+
+//true + false
+let var3 = true + false;
+console.log(var3);
+
+//6 / "3"
+let var4 = 6 / "3";
+console.log(var4);
+
+//"2" * "3"
+let var5 = "2" * "3";
+console.log(var5);
+
+//4 + 5 + "px"
+let var6 = 4 + 5 + "px";
+console.log(var6);
+
+//"$" + 4 + 5
+let var7 = "$" + 4 + 5;
+console.log(var7);
+
+//"4" - 2
+let var8 = "4" - 2;
+console.log(var8);
+
+//"4px" - 2
+let var9 = "4px" - 2;
+console.log("4px" - 2);
+
+//"  -9  " + 5
+let var10 = "  -9  " + 5;
+console.log(var10);
+
+//"  -9  " - 5
+let var11 = "  -9  " - 5;
+console.log(var11);
+
+//null + 1
+let var12 = null + 1;
+console.log(var12);
+
+//undefined + 1
+let var13 = undefined + 1
+console.log(undefined + 1);
+
+//" \t \n" - 2
+let var14 = " \t \n" - 2;
+console.log(var14);
