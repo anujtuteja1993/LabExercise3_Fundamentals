4f97687e384733388a089b365120edef7a8af108
Anuj Tuteja <anujtuteja@Anujs-MBP.fritz.box>
2022-08-29 21:19:44 +1200

    Question 2

--- Question2.js
+++ Question2.js
@@ -6,6 +6,6 @@
 //   alert(a + b); // 12
 
 
-let a = prompt("First number?", 1);
-let b = prompt("Second number?", 2);
-alert(a + b); // 12
+let a = parseInt(prompt("First number?", 1));
+let b = parseInt(prompt("Second number?", 2));
+alert(a + b);
